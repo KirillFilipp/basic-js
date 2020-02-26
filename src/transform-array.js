1d0485577ee6695a51db66ee7d3eeea2861943a7
KirillFilipp <kirill_filipp@mail.ru>
2020-02-26 21:42:41 +0300

    feat: add solution transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,27 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    let arrTrans = [];
+    if (!Array.isArray(arr)) {
+        throw new Error();
+    }
+    for (let index = 0; index < arr.length; index++) {
+        let e = arr[index];
+        if (e === '--discard-next') {
+            index = index + 1;
+        } else if (e === '--discard-prev') {
+            arrTrans.pop();
+        } else if (e === '--double-next') {
+            if (typeof arr[index + 1] !== 'undefined') {
+                arrTrans.push(arr[index + 1]);
+            } else { }
+        } else if (e === '--double-prev') {
+            if (typeof arr[index - 1] !== 'undefined') {
+                arrTrans.push(arr[index - 1]);
+            } else { }
+        } else {
+            if (typeof e !== 'undefined') {
+                arrTrans.push(e);
+            } else { }
+        }
+    }
+    return arrTrans;
 };
