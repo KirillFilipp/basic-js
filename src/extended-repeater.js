d21e9490639cedbee86fd900597b8d608bad8724
KirillFilipp <kirill_filipp@mail.ru>
2020-03-05 18:06:53 +0300

    feat: add basic solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,56 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let result = [];
+    const additionResult = [];
+    let string = str.toString();
+
+    let addition, additionRepeatTimes, separator, repeatTimes, additionSeparator;
+
+    if (options.additionSeparator && options.additionRepeatTimes > 0) {
+        additionSeparator = options.additionSeparator;
+    } else {
+        additionSeparator = '';
+    }
+
+    if (options.repeatTimes && !isNaN(options.repeatTimes)) {
+        repeatTimes = options.repeatTimes;
+    } else {
+        repeatTimes = 1;
+    }
+
+    if (options.additionRepeatTimes && !isNaN(options.additionRepeatTimes) ) {
+        additionRepeatTimes = options.additionRepeatTimes;
+    } else {
+        additionRepeatTimes = 1;
+    }
+
+    if (options.addition) {
+        addition = options.addition.toString();
+        for (let i = 0; i < additionRepeatTimes; i++) {
+            additionResult.push(addition.concat(additionSeparator));
+        }
+        addition = additionResult.join(additionSeparator);
+    } else {
+        addition = '';
+    }
+
+    if (options.separator) {
+        separator = options.separator;
+    } else {
+        separator = '+';
+    }
+
+
+
+
+
+
+    for (let i = 0; i < repeatTimes; i++) {
+        result.push(string.concat(addition));
+    }
+
+
+
+
+
+    return result.join(separator);
 };
-  
